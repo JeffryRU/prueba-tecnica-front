d9162eb8c44f1c40b9bc98426130d490e73d4197
Jeffry RU <jeffry.25.ru@gmail.com>
2026-09-22 02:38:09 -0500

    feat: rutas de detalle y publicación con navegación activa por sección

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router'
 import { NotFoundPage } from '@/pages/not-found/NotFoundPage'
 import { PokedexPage } from '@/pages/pokedex/PokedexPage'
+import { PokemonDetailPage } from '@/pages/pokemon-detail/PokemonDetailPage'
+import { NewPostPage } from '@/pages/posts/NewPostPage'
 import { MainLayout } from '@/shared/layouts/MainLayout'
 
 export const router = createBrowserRouter([
@@ -9,6 +11,8 @@ export const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { index: true, element: <PokedexPage /> },
+      { path: 'pokemon/:name', element: <PokemonDetailPage /> },
+      { path: 'posts/new', element: <NewPostPage /> },
       { path: '*', element: <NotFoundPage /> },
     ],
   },
